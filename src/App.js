e7b77dced63f9acaf70a4d00427bc6ce6fb9d51d
Idris Adams <idreezthor@gmail.com>
2022-02-14 01:41:09 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {CardList} from './components/card-list/card-list.component';
 import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor(){
     super();
     this.state = {
@@ -17,12 +17,12 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({ monsters: users}));
   }
-
+  
   render() {
     const { monsters , searchField} = this.state;
     const filteredMonsters = monsters.filter (monster => 
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-      );
+    );
 
     return (
       <div className="App">
